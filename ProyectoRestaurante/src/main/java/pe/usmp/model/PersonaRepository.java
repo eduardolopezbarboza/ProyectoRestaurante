00255d8698934ac403bffa87b01c70232ef46f35
Eduardo <Eduardo@DESKTOP-8GLRGK5>
2017-11-03 20:29:21 -0500

    version 0.5

--- ProyectoRestaurante/src/main/java/pe/usmp/model/PersonaRepository.java
+++ ProyectoRestaurante/src/main/java/pe/usmp/model/PersonaRepository.java
@@ -2,6 +2,7 @@ package pe.usmp.model;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.repository.Repository;
 
@@ -9,8 +10,9 @@ public interface PersonaRepository extends Repository<Persona, Long>{
 
 	void save(Persona persona) throws DataAccessException;
 	
+	@Cacheable("personas")
 	List<Persona> findAll();
 	
-	Persona findOne(Long id);
+	/*Persona findOne(Long id);*/
 }
 
